93f180eb9d6d9a2c217520089e9b78d0c1dcfb2d
Zhouyun <111507928+Phryel@users.noreply.github.com>
2023-01-18 14:48:37 +0700

    Add files via upload

---
+++ profile2.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <H2 style="text-align: center"><u>Profile</u></H2>
+        <table>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>Phryel Dave Bryant</td>
+            </tr>
+            <tr>
+                <td>Kelas </td>
+                <td>: </td>
+                <td>XI</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>: </td>
+                <td>RPL</td>
+            </tr>
+        </table>
+        <center><img src="dont-big.gif"></center>
+    </body>
+</html>
