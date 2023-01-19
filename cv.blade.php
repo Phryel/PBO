9a12c69374bc5e60a483485b4e3350b063a11405
Zhouyun <111507928+Phryel@users.noreply.github.com>
2023-01-19 09:12:07 +0700

    Add files via upload

---
+++ cv.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cv</title>
+    <link rel="stylesheet" href="Style_cv.css">
+</head>
+<body>
+        <table>
+            <tr>
+                <td> 
+                    <p>Dev</p> 
+                </td>
+                <td>
+                    <p>Phryel@gmail.com</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h1>Phryel Dave Bryant</h1>
+                </td>
+                <td>
+                    <img class="profile" src="profile.webp">
+                </td>                
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td class="p-text">
+                    Profile
+                </td>
+                <td rowspan="2" class="i-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum assumenda a inventore doloremque harum eveniet id voluptas deleniti eum iusto! Illum at magnam corporis reprehenderit, corrupti quaerat doloremque accusamus ut! </p>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+
+            </tr>
+        </table>
+</body>
+</html>
--- web.php
+++ web.php
@@ -17,47 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return ('Halaman Home');
-});
-
-Route::get('/about', function () {
-    return ('Halaman About');
-});
-
-Route::get('/blog', function () {
-    return ('Halaman Blog');
-});
-
-Route::get('/source', function () {
-    return ('Halaman Source');
-});
-
-route::get('/profil', function () {
-    return ('Halaman Profil');
-});
-
-route ::get('/belajar', function () {
-    echo '<h1>Hello World</h1>';
-    echo '<p>Sedang Belajar Larevel</p>';
-});
-
-route::get('/siswa/smkbpi/rpl', function () {
-    echo '<h2 style="text-align: center"><u>Welcome to 11 RPL</u></h2>';
-});
-
-route::get('/siswa/{phryel}', function ($phryel) {
-    return "Tampilan data siswa bernama $phryel";
-});
-
-route::get('/stok_barang/{jenis}/{merek}', function($jenis,$merek) {
-    return "Cek sisa stok untuk $jenis $merek";
-});
-
-route::get('/profile', function() {
-    return view('profile');
-});
-
-route::get('/profile2', function() {
-    return view('profile2');
+route::get('/cv_phryel', function () {
+    return view('cv');
 });
